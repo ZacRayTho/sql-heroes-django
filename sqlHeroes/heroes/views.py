6d18ad275856a18f695183e8f0a8682873053c73
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-03-29 15:05:26 +0000

    added view with list of all heroes

--- sqlHeroes/heroes/views.py
+++ sqlHeroes/heroes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Hero, Power, Relationship_type, Relationship
 
+def heroes(request):
+    x = Hero.objects.all()
+    list = []
+    for item in x:
+        list.append(item.name)
+    # print(list)
+    return HttpResponse("All Heroes: %s " % list)
 # Create your views here.
